4371546a278fe7a034594b195fb9a138d86b5223
morganwang010 <morganwang010@sina.com>
2014-06-02 09:45:06 +0800

    the convolution example and change a RGB to gray

---
+++ filter2D.cpp
@@ -0,0 +1,64 @@
+// filter2D.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+/** @function main */
+int main ( int argc, char** argv )
+{
+  /// Declare variables
+  Mat src, dst;
+
+  Mat kernel;
+  Point anchor;
+  double delta;
+  int ddepth;
+  int kernel_size;
+  char* window_name = "filter2D Demo";
+
+  int c;
+  const char* imagefile = "E:/33.jpg";
+  /// Load an image
+  src = imread( imagefile );
+  Mat gray_image;
+  cvtColor( src, gray_image, CV_BGR2GRAY );//change the RGB to gray pic--by morgan
+  imshow("gray window",gray_image);
+  if( !src.data )
+  { return -1; }
+
+  /// Create window
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+
+  /// Initialize arguments for the filter
+  anchor = Point( -1, -1 );
+  delta = 0;
+  ddepth = -1;
+
+  /// Loop - Will filter the image with different kernel sizes each 0.5 seconds
+  int ind = 0;
+  while( true )
+    {
+      c = waitKey(500);
+      /// Press 'ESC' to exit the program
+      if( (char)c == 27 )
+        { break; }
+
+      /// Update kernel size for a normalized box filter
+      kernel_size = 3 + 2*( ind%5 );
+      kernel = Mat::ones( kernel_size, kernel_size, CV_32F )/ (float)(kernel_size*kernel_size);
+
+      /// Apply filter
+      filter2D(src, dst, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
+      imshow( window_name, dst );
+      ind++;
+    }
+
+  return 0;
+}
